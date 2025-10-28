95733ab68275b0b4f73115c9ed1b077180a279fe
Mr-XX23 <starbalami00@gmail.com>
2025-10-28 23:45:56 +0545

    Refactor build script in package.json and remove unused MCPService methods

--- MCP_Client/services.ts
+++ MCP_Client/services.ts
@@ -1,57 +1,5 @@
-import { MessageParam, Tool, } from "@anthropic-ai/sdk/resources/messages/messages.mjs";
-import readline from "readline/promises";
 
 
-class MCPService {
-    async processQuery(query: string) {
-        const messages: MessageParam[] = [
-            {
-                role: "user",
-                content: query,
-            },
-        ];
+export class MCPService {
 
-        const response = await this.anthropic.messages.create({
-            model: "claude-3-5-sonnet-20241022",
-            max_tokens: 1000,
-            messages,
-            tools: this.tools,
-        });
-
-        const finalText = [];
-
-        for (const content of response.content) {
-            if (content.type === "text") {
-                finalText.push(content.text);
-            } else if (content.type === "tool_use") {
-                const toolName = content.name;
-                const toolArgs = content.input as { [x: string]: unknown } | undefined;
-
-                const result = await this.mcp.callTool({
-                    name: toolName,
-                    arguments: toolArgs,
-                });
-                finalText.push(
-                    `[Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}]`
-                );
-
-                messages.push({
-                    role: "user",
-                    content: result.content as string,
-                });
-
-                const response = await this.anthropic.messages.create({
-                    model: "claude-3-5-sonnet-20241022",
-                    max_tokens: 1000,
-                    messages,
-                });
-
-                finalText.push(
-                    response.content[0].type === "text" ? response.content[0].text : ""
-                );
-            }
-        }
-
-        return finalText.join("\n");
-    }
 }
